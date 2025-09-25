40cd8b400be3a91a60bc97df973078bbb42536d6
ReneS1981 <rene.schwarzenberger@icloud.com>
2025-09-25 19:23:01 +0200

    3

--- data.js
+++ data.js
@@ -59,7 +59,7 @@ var APP_DATA = {
           "yaw": -0.0566016450045943,
           "pitch": 0.008849135873035152,
           "title": "WC EG",
-          "text": "<img source src=https://raw.githubusercontent.com/ReneS1981/virtueller-rundgang/refs/heads/main/img/WC%20EG.jpg"
+          "text": "<img src=https://raw.githubusercontent.com/ReneS1981/virtueller-rundgang/main/img/WC%20EG.jpg´>"
         },
         {
           "yaw": 0.1926691111702432,
